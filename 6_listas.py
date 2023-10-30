a4943f175f64bdc1827669d03b8661acdad481ef
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-10-30 02:56:58 -0300

    6_listas.py

--- 6_listas.py
+++ 6_listas.py
@@ -20,3 +20,21 @@ print(lista_numeros[2])
 numeros = [1, 2, 3]
 strings = ["Maria", "João", "José"]
 decimais = [10.5, 15.3, 30.5]
+
+#Metodos com Listas (append):
+
+lista_vazia = []
+
+lista_vazia.append("Olá")
+lista_vazia.append("Mundo")
+
+print(lista_vazia)
+
+#Metodos com Listas (len, min, max) Funções Nativas:
+
+numeros = [10, 9, 8, 7, 6]
+
+print( "total", len(numeros))
+print( "menor valor", min(numeros))
+print( "maior valor", max(numeros))
+
