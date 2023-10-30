ed35c0a6680c6253bfdbd7111a1ce3f1fe14c586
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-10-30 02:12:08 -0300

    operadores.py

---
+++ 3_operadores.py
@@ -0,0 +1,9 @@
+soma = 1 + 1
+multiplicacao = 2 * 2
+divisao = 20 / 20
+potencia = 8 ** 2
+
+print("soma", soma)
+print("multiplicacao", multiplicacao)
+print("divisao", divisao)
+print("potencia", potencia)
