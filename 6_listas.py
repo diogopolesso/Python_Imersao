f3cfd6d065316d3261c7180f655cf9a25a219e6f
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-10-30 02:43:52 -0300

    listas.py

---
+++ 6_listas.py
@@ -0,0 +1,22 @@
+lista_numeros = [1, 2, 3]
+
+print(lista_numeros[0])
+print(lista_numeros[1])
+print(lista_numeros[2])
+
+
+#Pdemos Manipular Listas:
+
+lista_numeros = [1, 2, 3]
+
+listas_numeros[0] = 5  #acesso a posição 0 e atribuo valor 5
+
+print(lista_numeros[0])
+print(lista_numeros[1])
+print(lista_numeros[2])
+
+#Listas Podem ter Diferentes dados como:
+
+numeros = [1, 2, 3]
+strings = ["Maria", "João", "José"]
+decimais = [10.5, 15.3, 30.5]
--- listas.py
+++
@@ -1,5 +0,0 @@
-lista_numeros = [1, 2, 3]
-
-print(lista_numeros[0])
-print(lista_numeros[1])
-print(lista_numeros[2])
