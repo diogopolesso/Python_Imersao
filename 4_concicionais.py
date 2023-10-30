c95aece8aed31d2c7490e7522b25be7b383b21cc
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-10-30 02:22:39 -0300

    concicionais.py

---
+++ 4_concicionais.py
@@ -0,0 +1,17 @@
+valor_1 = 20
+valor_2 = 50
+
+if valor_1 > valor_2
+    print(valor_1 "É Maior que ", valor_2)
+else:
+    print(valor_2 "É Maior que ", valor_1)
+
+
+# Pratica:
+
+idade = int(input("Qual a Sua Idade?"))
+
+if idade >= 18:
+    print("ENTRADADA PERMITIDA!!")
+else:
+    print("ENTRADA NÃO PERMITIDA!!")
