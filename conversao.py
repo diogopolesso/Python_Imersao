b711574f9c8c6e849623b0b7b4bd9c32cca3cfd9
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-10-30 02:05:15 -0300

    Add files via upload

---
+++ conversao.py
@@ -0,0 +1,7 @@
+nome = input("Digite Seu Nome:")
+idade = int(input("Digite Sua Idade:"))
+peso = float(input("Digite Seu Peso:"))
+
+print("Nome: " + nome)
+print("Tem " + idade + " anos de Idade")
+print("Pesa: " + peso + "Kg")
