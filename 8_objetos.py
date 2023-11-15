ea888694da2977723a7b526c7b429b6741083c67
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-11-15 01:57:08 -0300

    8_objetos.py

---
+++ 8_objetos.py
@@ -0,0 +1,34 @@
+# Objetos usam chave e valor para armazenar informações:
+
+variavel = {
+  "chave": "valor",
+}
+
+# Exemplo 1:
+
+aluno = {
+  "nome": "Programador Python",
+  "idade": 38,
+  "peso": 79.5
+}
+
+print(aluno ['nome'])
+print(aluno ['idade'])
+print(aluno ['peso'])
+
+# Exemplo 2:
+
+Jogador = {
+  "nome": "Diogo",
+  "nivel": 1,
+  "hp": 100,
+  "exp": 2,
+  "dano": 10,
+}
+
+npcs = [
+  { "nome": "Mostrinho", "dano": 2, "hp": 50, "exp": 5 },
+  { "nome": "Mostro", "dano": 5, "hp": 100, "exp": 10 },
+  { "nome": "Mostrão", "dano": 10, "hp": 150, "exp": 20 },
+  { "nome": "Chefe", "dano": 20, "hp": 300, "exp": 30 },
+]
