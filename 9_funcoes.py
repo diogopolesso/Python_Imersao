d9158425a996f8b63dcbd25176cc0d2e4dea6445
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-11-15 02:25:07 -0300

    9_funcoes.py

--- 9_funcoes.py
+++ 9_funcoes.py
@@ -1,2 +1,50 @@
 # Funções são Blocos reutilizaveis de codigo que utilizam tarefas especificas
 
+
+ 3
+# cadigo sem funcaa
+fluxo_caixa
+printt"---------")
+5
+print("@ Fluxo caixa")
+printt"---------"-")
+PO2HARIRRRRRNRXRRRRRRASRXRRARR 4
+print("1 - Adicionar
+print("2 - Adicionar
+printl"In# Digite
+while True:
+opcao = int(
+if opcao =e
+nome =
+valor
+elif opcao
+nome
+valor
+fluxo
+A
+else:
+th nota
+total
+for fc
+receita")
+despesa")
+outro numero para encerrar #\n")
+input("Digite a opcao: ") )
+1:
+input("Nome: ")
+= float( input("Valor: "))
+fluxo_calxa.append((
+"nome": nome.
+"valor": valor
+=2:
+= input("Nome: ")
+= float( input("Valor: "))
+Ca1xa.append(
+"nome": nome,
+"valor": valor
+break
+fiscal
+in fluxo_caixa:
+print("Nome:", fc['nome'l, ", Valor: R$", tel'valar'!)
+total total N fel'valor']
+print["Saldo atual: R$", total
