51f6386034aacfc79fa62c0ddfdd0f556f742da7
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-12-05 00:16:51 -0300

    Tipos Primitivos e Saída de Dados

---
+++ 9_TiposPrimitivos.py
@@ -0,0 +1,8 @@
+int = 'Números Inteiros' [7, -4, 0, 1985]
+float = 'Números Reais ou Flutuantes' [0.5, 0.755, -15.000, 7.0]
+bool = 'Valores Lógicos ou Boleanos' [true = Verdadero, false = Falso]
+str =  'strings ou textos' ['Olá Mundo', '1985', '']
+
+
+n1 = int(input('Digite um Número')) #convertemos tudo que esta na string para números inteiros
+n2 = int(input('Digite mais um numero'))
